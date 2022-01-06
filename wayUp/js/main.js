6adc6603ade1d816b1c10a900694481553d55711
Siarheispivak <siarheispivak2@gmail.com>
2022-01-06 22:28:24 +0300

    new2

--- wayUp/js/main.js
+++ wayUp/js/main.js
@@ -72,7 +72,83 @@
 
 
 
+// //Домашнее заданиие ко второму уроку!!!!!!
+// // 1.
+// for (let i = 10; i <= 50; i += 2) {
+//     console.log(i);
+// }
+
+// // 2.
+// let mySelf = {
+//     name: "Siarhei",
+//     surname: "Spivak",
+//     age: 28,
+//     hasDomesticAnimal: true
+// }
+// console.log(mySelf);
 
 
 
 
+
+
+// // 3.
+// const array = [
+//     'я в Симбирск,',
+//     'в трактире.',
+//     'с утра',
+//     'В ту же ночь',
+//     'Я остановился',
+//     'для закупки',
+//     'что и было поручено Савельичу.',
+//     'приехал',
+//     'где должен был',
+//     'нужных вещей',
+//     'отправился по лавкам.',
+//     'пробыть сутки',
+//     'Савельич'
+// ]
+
+// let a = array[3];
+// let b = array[7];
+// let c = array[0];
+// let d = array[8];
+// let e = array[11];
+// let f = array[5];
+// let g = array[9];
+// let h = array[6];
+// let i = array[4];
+// let j = array[1];
+// let k = array[12];
+// let l = array[2];
+// let m = array[10];
+
+
+// let result = a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + h + " " + i + " " + j + " " + k + " " + l + " " + m;
+// console.log(result);
+
+// или
+
+// let result = `${a} ${b} ${c} ${d} ${e} ${f} ${g} ${h} ${i} ${j} ${k} ${l} ${m}`;
+// console.log(result);
+
+
+
+// // 4.
+// function allNames(firstName, lastName) {
+
+//     const fullName = `${firstName} ${lastName}`;
+//     console.log(fullName);
+// }
+
+
+// allNames('Siarhei', 'Spivak');
+
+
+
+// // 5.
+// let i = 21;
+// while (i <= 67) {
+//     i += 2;
+//     console.log(i);
+// }
