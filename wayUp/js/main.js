1fd9884bc5a431cbb457cad7c78f42e26fd2c45f
Siarheispivak <siarheispivak2@gmail.com>
2022-01-06 20:42:16 +0300

    wayUp

---
+++ wayUp/js/main.js
@@ -0,0 +1,92 @@
+// // 1.
+// let cityInfo;
+// let cityName = "Minsk";
+// let country = "Belarus";
+// let numPopulation = 9000000;
+// let cityStadium = true;
+// cityInfo = `${cityName} ${country} ${numPopulation} ${cityStadium}`;
+// console.log(cityInfo);
+
+
+
+// // 1.1 по-другому
+// let cityInfo;
+// let cityName = "Minsk";
+// let country = "Belarus";
+// let numPopulation = 9000000;
+// let cityStadium = true;
+// cityInfo = cityName + " " + country + " " + numPopulation + " " + cityStadium;
+// console.log(cityInfo);
+
+
+
+// // 2.
+// let hight = 40;
+// let width = 70;
+// let area = hight * width;
+// console.log(area);
+
+
+
+
+// // 3.
+
+// let time = 2;
+// let speedOfFirst = 95;
+// let speedOfSecond = 114;
+// let result = (speedOfFirst + speedOfSecond) * time;
+// console.log(result);
+
+
+
+// // 4. if
+// const randomNumber = Math.floor(Math.random() * 100);
+// if (randomNumber < 20) {
+//     console.log("randomNumber меньше 20")
+// } else if (randomNumber > 50) {
+//     console.log("randomNumber больше 50")
+// } else {
+//     console.log("randomNumber больше 20, и меньше 50")
+// }
+
+// // 5. switch case
+// const randomNumber = Math.floor(Math.random() * 100);
+// let a = randomNumber < 20;
+// let b = randomNumber > 50;
+// // console.log(randomNumber);
+// // console.log(a);
+// // console.log(b);
+// switch (true) {
+//     case a:
+//         console.log("randomNumber меньше 20");
+//         break;
+//     case b:
+//         console.log("randomNumber больше 50");
+//         break;
+//     default:
+//         console.log("randomNumber больше 20, и меньше 50");
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+// 2 урок
+// // 1.
+// for (let i = 10; i <= 50; i += 2) {
+//     console.log(i);
+// }
+
+// 2.
+let mySelf = {
+    name: "Siarhei",
+    surname: "Spivak",
+    age: 28,
+    hasDomesticAnimal: true
+}
